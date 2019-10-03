8f5747e6ddc73210b43b2dac9ac802082046f16c
VcJmHg <zhaojianheng618@163.com>
2019-10-03 15:05:31 +0800

    对邮件发送功能进行简单整合

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -2,15 +2,41 @@ package main;
 
 import email.MailUtils;
 import org.apache.commons.mail.EmailException;
+import pojo.DidaCalendar;
+import utils.DidaUtils;
 
 import javax.mail.internet.AddressException;
 import java.io.*;
+import java.util.List;
 
 /**
  * GDTHelper的入口类
  */
 public class App {
     public static void main(String[] args) {
+        DidaUtils didaUtils=new DidaUtils();
+        List<DidaCalendar> didaCalendarList = null;
+        try {
+            didaCalendarList = didaUtils.getCalenderBeforeWeek();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //在控制台将日程信息按照完成的时间先后顺序进行输出
+        String ctime = null;
+        for(DidaCalendar didaCalendar:didaCalendarList){
+//            System.out.println(didaCalendar);
+            if(ctime==null){
+                ctime = didaCalendar.getDueDate().substring(0, 10);
+            }
+            if(ctime.equals(didaCalendar.getDueDate().substring(0,10) )){
+                System.out.println(didaCalendar.getTitle()+"   Date"+didaCalendar.getDueDate());
+            }else{
+                System.out.println("=================================");
+                ctime=didaCalendar.getDueDate().substring(0,10);
+                System.out.println(didaCalendar.getTitle()+"   Date"+didaCalendar.getDueDate());
+            }
+        }
         MailUtils mailUtils=new MailUtils("13069648310@163.com","VcJmHg0903","smtp.163.com");
         StringBuffer htmlContent=new StringBuffer();
         BufferedReader reader=null;
